3de28eb9c01f9b4913b8f4f820618e30c39068bf
karamage <karamage.umauma@gmail.com>
2020-07-06 15:20:35 +0900

    Hello composition Api

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,21 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <Hello></Hello>
+    <!-- img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/ -->
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+//import HelloWorld from '@/components/HelloWorld.vue';
+import Hello from "@/components/Hello.vue"
 
 export default Vue.extend({
   name: 'App',
   components: {
-    HelloWorld
+    //HelloWorld
+    Hello
   }
 });
 </script>
---
+++ src/components/Hello.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    Hello Composition Api
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "@vue/composition-api"
+
+export default defineComponent({
+  setup() {
+    // ここにリアクティブなデータ、関数を定義
+  }
+})
+</script>
