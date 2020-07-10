7efb8c6f218168a9538d59f1a96f9988b97be7be
karamage <karamage.umauma@gmail.com>
2020-07-10 17:21:29 +0900

    add usePagination

---
+++ src/composables/use-pagination.ts
@@ -0,0 +1,63 @@
+import { computed, ref, toRefs, watch } from "@vue/composition-api"
+
+export default function usePagination(options = {}) {
+  const { perPage = ref(10), total = ref(0), startPage = 1 } = toRefs(
+    options
+  )
+
+  // Internal currentPage value
+  const _currentPage = ref(startPage)
+  // public readonly ref for the currentPage
+  // changing the current Page is only possible through the provide methods (see below)
+  const currentPage = computed(() => _currentPage.value)
+
+  // Computed values
+  const lastPage = computed(() =>
+    total.value ? Math.ceil(total.value / perPage.value) : 0
+  )
+  const offset = computed(() =>
+    Math.min((currentPage.value - 1) * perPage.value, total.value)
+  )
+
+  // Functions
+  const set = (val:number) => {
+    if (typeof val !== 'number') return
+    _currentPage.value = minmax(val, 1, lastPage.value)
+  }
+  const prev = () => set(currentPage.value - 1)
+  const next = () => set(currentPage.value + 1)
+  const first = () => set(1)
+  const last = () => set(lastPage.value)
+
+  watch(
+    [total, perPage],
+    () => {
+      if (_currentPage.value > lastPage.value) {
+        _currentPage.value = lastPage.value
+      }
+    },
+    //{ lazy: true } // default true
+  )
+
+  return {
+    // Mutable state
+    perPage,
+    total,
+    // Computed
+    currentPage,
+    lastPage,
+    offset,
+    // Funstions
+    next,
+    prev,
+    first,
+    last,
+    set,
+  }
+}
+
+function minmax(val:number, min:number, max:number) {
+  if (val < min) return min
+  if (val > max) return max
+  return val
+}
